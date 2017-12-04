7ded139d91727d9974d15037eb4b28d282f2870a
RainerEmrich <rainer@emrich-ebersheim.de>
2017-12-04 09:54:32 +0000

    Install seccomp for Debian 8 systems.
    	modified:   scripts/upgrade.system.sh

--- scripts/upgrade.system.sh
+++ scripts/upgrade.system.sh
@@ -48,19 +48,21 @@ upgrade_system () {
 		Debian)
 			case ${DIST_RELEASE} in
 			8.*)
+				sudo apt-get install seccomp libseccomp-dev -y
+
 				wget -qO- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
 				echo "deb https://deb.nodesource.com/node_6.x ${DIST_CODENAME} main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 				echo "deb-src https://deb.nodesource.com/node_6.x ${DIST_CODENAME} main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
 				sudo apt-get update
-				sudo apt-get install nodejs
+				sudo apt-get install nodejs -y
 				;;
 			*)
-				sudo apt-get install npm nodejs-legacy
+				sudo apt-get install npm nodejs-legacy -y
 				;;
 			esac
 			;;
 		*)
-			sudo apt-get install npm nodejs-legacy
+			sudo apt-get install npm nodejs-legacy -y
 			;;
 		esac
 
