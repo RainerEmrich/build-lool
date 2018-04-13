15a4aadf728effb6bde7c5fe5f9a78910af9f138
RainerEmrich <rainer@emrich-ebersheim.de>
2018-04-13 08:14:48 +0000

    Update copyright notice.
    	modified:   build-lool.sh
    	modified:   config/lool-config.sh.example
    	modified:   scripts/ask.to.continue.sh
    	modified:   scripts/build.libreoffice.sh
    	modified:   scripts/build.online.sh
    	modified:   scripts/build.package.sh
    	modified:   scripts/build.poco.sh
    	modified:   scripts/enable.source.repositories.sh
    	modified:   scripts/get.config.sh
    	modified:   scripts/get.os.release.sh
    	modified:   scripts/get.sources.sh
    	modified:   scripts/pause.sh
    	modified:   scripts/setup.functions.sh
    	modified:   scripts/show.info.sh
    	modified:   scripts/test.os.release.sh
    	modified:   scripts/upgrade.system.sh

--- scripts/upgrade.system.sh
+++ scripts/upgrade.system.sh
@@ -2,7 +2,7 @@
 #
 # Update the system and install the required packages.
 #
-# Copyright 2017, 2018 Rainer Emrich, <rainer@emrich-ebersheim.de>
+# Copyright (C) 2017-2018 Rainer Emrich, <rainer@emrich-ebersheim.de>
 #
 # This file is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
